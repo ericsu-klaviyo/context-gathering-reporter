1769b18f6a4fb8b0f872facb1a9c13e063ce9852
Eric Su <eric.su@klaviyo.com>
2024-04-09 09:59:47 -0400

    Conform file naming to provided report names

--- constants.py
+++ constants.py
@@ -2,12 +2,14 @@ TABLE_HEADERS = ["Domain", "# Missing", "%"]
 TABLE_COLALIGN = ("left","right","right")
 TOTAL_STR = "Total"
 
-FLOWS_INTERFACE_TEAM_NAME = "Interface"
 FLOWS_IM_TEAM_NAME = "Insight & Management"
+FLOWS_INTERFACE_TEAM_NAME = "Interface"
 FLOWS_PLATFORM_TEAM_NAME = "Platform"
 
 TEAM_NAME_TO_FILE_NAME = {
-    FLOWS_IM_TEAM_NAME: "flims"
+    FLOWS_IM_TEAM_NAME: "flows-insight-management",
+    FLOWS_INTERFACE_TEAM_NAME: "information-architecture",
+    FLOWS_PLATFORM_TEAM_NAME: "flows-platform",
 }
 
 DOMAIN_OWNERSHIP = {
